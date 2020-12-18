96c7cb1b113d0e54c326389ff2c0f9489f23c3f0
zeren_he <he.zeren@asiainfo-sec.com>
2020-12-18 15:53:41 +0800

    leetcode刷题

---
+++ src/JianZhiOffer40/Solution.java
@@ -0,0 +1,14 @@
+package JianZhiOffer40;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        int[] vec = new int[k];
+        Arrays.sort(arr);
+        for (int i = 0; i < k; ++i) {
+            vec[i] = arr[i];
+        }
+        return vec;
+    }
+}
